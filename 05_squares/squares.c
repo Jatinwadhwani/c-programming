7e669c37c95cbaf90f2a07045eb261a8ba9e2908
Coursera Learner <nobody@nowhere.nul>
2019-07-24 01:02:41 +0000

    squares assignment

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -7,6 +7,9 @@
  * offset (INCLUSIVE) and offset + size (EXCLUSIVE)
  */
 int isInRange(int coord, int offset, int size) {
+  if ((coord >= offset) && (coord < (offset + size))){
+    return 1;
+  }
   // if coord is in range, return 1
   // else, return 0
   return 0;
@@ -17,12 +20,47 @@ int isInRange(int coord, int offset, int size) {
  * offset + size
  */
 int isAtBorder(int coord, int offset, int size) {
+  if ((coord == offset) || (coord == (offset + size))){
+    return 1;
+  }
   // if coord is equal to offest or offset + size
   // return 1, else return 0
   return 0;
 }
 
+int max(int n1, int n2){
+  if (n1 > n2) {
+    return n1;
+  }
+  return n2;
+
+}
+
 void squares(int size1, int x_offset, int y_offset, int size2) {
+  int w = max(size1, (x_offset + size2));
+  int h = max(size1, (y_offset + size2));
+  for (int y = 0; y < h; y++){
+    for (int x = 0; x < w; x++) {
+      if ((isInRange(x, x_offset, size2) == 1) && (isAtBorder(y, y_offset, (size2-1)) == 1)) {
+	  printf("*");
+
+      }
+      else if ((isInRange(y, y_offset, size2) == 1) && (isAtBorder(x, x_offset, (size2-1)) == 1)) {
+	  printf("*");
+      }
+      else if ((x < size1) && ((y == 0) || (y == (size1-1)))){
+	  printf("#");
+      }
+      else if ((y < size1) && ((x  == 0) || (x == (size1-1)))){
+	printf("#");
+      }
+      else {
+	printf(" ");
+      }
+    }
+    printf("\n");
+  }
+	
   //compute the max of size1 and (x_offset + size2).  Call this w
 
   //compute the max of size1 and (y_offset + size2).  Call this h
